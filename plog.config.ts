a54b71bd12d6336304808060c64a87619edd131d
luckypeppa <871565198@qq.com>
2023-01-01 22:12:15 +0800

    change(Card.vue): show relative time when a blog was created in 7 days.

--- plog.config.ts
+++ plog.config.ts
@@ -3,15 +3,15 @@ const appConfig: AppConfig = {
   sidebar: {
     domain: {
       text: "How to buy a domain in Namecheap?",
-      link: "/docs/domain/buy-domain"
+      link: "/docs/domain/buy-domain",
     },
   },
   home: [
     {
       text: "How to buy a domain in Namecheap?",
       link: "/docs/domain/buy-domain",
-      createdAt: "2023-1-1T18:40",
-    cover: "/images/namecheap.png",
+      createdAt: "2023-1-1T10:40",
+      cover: "/images/namecheap.png",
     },
   ],
 };
