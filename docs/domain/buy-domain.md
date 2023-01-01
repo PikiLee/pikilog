e26d0bbfb8f189b4c9b44d9be54ab930b27a01db
luckypeppa <871565198@qq.com>
2023-01-01 19:10:02 +0800

    First blog.

---
+++ docs/domain/buy-domain.md
@@ -0,0 +1,22 @@
+# Instructions
+
+## Go to [Namecheap](https://www.namecheap.com/)
+![The home page of Namecheap](./images/namecheap.png)
+
+## Input the domain you want to buy
+Namecheap will display a list of domains. Some of the domains have been purchased by someone. And still there are many domains avaiable for sale.
+![A list of domains](./images/ancat.png)
+
+## Pick a domain you are interested in.
+Here I pick ancat.art for example. Click *Add To Cart* and then *Checkout*
+![Add to cart](./images/addToCart.png)
+
+## Click confirm order.
+![Confirm order](./images/confirmOrder.png)
+## Login if you have had a account or sign up.
+![Login](./images/login.png)
+
+## Type in your credit card info and click *Pay Now*.
+![Payment Info](./images/payment.png)
+Whoops! You hava a domain now.
+
--- plog.config.ts
+++ plog.config.ts
@@ -1,33 +1,17 @@
 import { AppConfig } from "./types/app";
 const appConfig: AppConfig = {
   sidebar: {
-    guide: {
-      text: "Guide",
-      items: [
-        { text: "Introduction", link: "/docs/guide/introduction" },
-        { text: "part1 and good one", link: "/docs/guide/part1" },
-        {
-          text: "coll",
-          items: [
-            { text: "gool", link: "/docs/guide/coll/goll" },
-            { text: "why it is so good", link: "/docs/guide/coll/why" },
-          ],
-        },
-      ],
+    domain: {
+      text: "How to buy a domain in Namecheap?",
+      link: "/docs/domain/buy-domain"
     },
   },
   home: [
     {
-      text: "Guide",
-      link: "/docs/guide/introduction",
-      createdAt: "2022-12-23T12:00",
-      cover: "/images/icon1.jpg",
-    },
-    {
-      text: "Guide-part1",
-      link: "/docs/guide/part1",
-      createdAt: "2022-12-23T12:00",
-      cover: "/images/f.jpg",
+      text: "How to buy a domain in Namecheap?",
+      link: "/docs/domain/buy-domain",
+      createdAt: "2023-1-1T18:40",
+    cover: "/images/namecheap.png",
     },
   ],
 };
