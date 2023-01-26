c1fb26aa5c637b5082b51d43bac30e5f4b10024e
luckypeppa <871565198@qq.com>
2023-01-26 14:18:09 +0800

    feat(app): add footer

---
+++ components/app/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="footer">
+        Piki's Blog
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+    display: grid;
+    place-items: center;
+    min-height: 20vh;
+    font-size: 2rem;
+    border-top: 1px solid rgba(#555, 0.5);
+    margin-top: 4vh;
+}
+</style>
